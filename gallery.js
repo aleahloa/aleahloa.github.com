05e7dcc63a54557f62785e79a25230a2edb0ba95
Jonny <myself@jonny.li>
2014-06-02 13:53:44 -0700

    Added author field to gallery images

--- gallery.js
+++ gallery.js
@@ -2,6 +2,7 @@ function GalleryCtrl($scope) {
     $scope.images = [
         {
             title: "Test",
+            author: "Anonymous Artist",
             src: "http://shoplhslv.com/images/testpattern001.png",
             caption: "insert caption here"
         }
