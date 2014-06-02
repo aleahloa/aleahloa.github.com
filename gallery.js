3a912134c0c5089172290f24ecab6fdf1ecf66e7
Jonny <myself@jonny.li>
2014-06-02 10:02:18 -0700

    Gallery page working now

---
+++ gallery.js
@@ -0,0 +1,9 @@
+function GalleryCtrl($scope) {
+    $scope.images = [
+        {
+            title: "Test",
+            src: "http://shoplhslv.com/images/testpattern001.png",
+            caption: "insert caption here"
+        }
+    ];
+}
