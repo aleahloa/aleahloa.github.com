79064ce569aea4dbfea3de8efb07ebe32d1837fd
aleahloa <aleahloa@yahoo.ca>
2014-06-02 22:46:01 -0700

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -34,6 +34,49 @@ function GalleryCtrl($scope) {
             src: "https://scontent-b-sea.xx.fbcdn.net/hphotos-xfa1/t1.0-9/488150_4394334106398_428929546_n.jpg",
             caption: "July 18th, 2013"
         }
+        
+        {
+             title: "Cotton Candy",
+            author: "Aleah Loa",
+            src: "https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-xpf1/t31.0-8/s720x720/324693_2251028325093_293588_o.jpg",
+            caption: "August 22nd, 2011"
+        }
+
+{
+             title: "Adhesion",
+            author: "Aleah Loa",
+            src: "https://scontent-b-sea.xx.fbcdn.net/hphotos-xfa1/t31.0-8/s720x720/280065_2199938687884_2142537_o.jpg",
+            caption: "August 1st, 2011"
+        }
+        
+        {
+             title: "Midnight City",
+            author: "Aleah Loa",
+            src: "https://scontent-a-sea.xx.fbcdn.net/hphotos-xfp1/t31.0-8/s720x720/169346_4010105900933_1933762236_o.jpg",
+            caption: "June 15th, 2012"
+        }
+        
+         {
+             title: "Sleepy Prayers",
+            author: "Aleah Loa",
+            src: "https://scontent-a-sea.xx.fbcdn.net/hphotos-xaf1/t31.0-8/s720x720/192804_4370784637676_1422868768_o.jpg",
+            caption: "September 3rd, 2012"
+        }
+        
+         {
+             title: "Mainstream on Main Street",
+            author: "Aleah Loa",
+            src: "https://scontent-b-sea.xx.fbcdn.net/hphotos-xfa1/t31.0-8/s720x720/192970_4370785437696_176576210_o.jpg",
+            caption: "September 3rd, 2012"
+        }
+        
+        {
+             title: "English Breakfast",
+            author: "Aleah Loa",
+            src: "https://scontent-b-sea.xx.fbcdn.net/hphotos-ash2/t31.0-8/q71/s720x720/1074743_10201394490574112_757581483_o.jpg",
+            caption: "July 18th, 2013"
+        }
+        
     ];
     ];
 }
