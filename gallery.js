088d74f3283c399846e455d6b3d5fa883f483c66
aleahloa <aleahloa@yahoo.ca>
2014-06-02 21:12:07 -0700

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,10 +1,10 @@
 function GalleryCtrl($scope) {
     $scope.images = [
         {
-            title: "Test",
-            author: "Anonymous Artist",
-            src: "http://shoplhslv.com/images/testpattern001.png",
-            caption: "insert caption here"
+            title: "Bubblegum Jets",
+            author: "Aleah Loa",
+            src: "https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-xaf1/t31.0-8/10339318_10203746067402063_1630967137937098385_o.jpg",
+            caption: "Bubblegum Jets"
         }
     ];
 }
