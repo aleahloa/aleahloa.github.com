abd0337383c9884e0674153888cf1da90fff39dd
aleahloa <aleahloa@yahoo.ca>
2014-06-02 22:28:46 -0700

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -6,5 +6,34 @@ function GalleryCtrl($scope) {
             src: "https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-xaf1/t31.0-8/10339318_10203746067402063_1630967137937098385_o.jpg",
             caption: "May 2nd, 2014"
         }
+        
+        {
+            title: "Parisian Nights",
+            author: "Aleah Loa",
+            src: "https://scontent-b-sea.xx.fbcdn.net/hphotos-xaf1/t1.0-9/1069960_10201394272448659_847322368_n.jpg",
+            caption: "July 18th, 2013"
+        }
+        
+        {
+            title: "Cathédrale Notre Dame de Paris",
+            author: "Aleah Loa",
+            src: "https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-xfa1/t1.0-9/998867_10201394491734141_1744311596_n.jpg",
+            caption: "July 18th, 2013"
+        }
+        
+        {
+            title: "Arènes d'Arles",
+            author: "Aleah Loa",
+            src: "https://scontent-a-sea.xx.fbcdn.net/hphotos-xap1/t31.0-8/q71/s720x720/1077044_10201394273648689_138833029_o.jpg",
+            caption: "July 18th, 2013"
+        }
+        
+        {
+            title: "Let's Call Banksy",
+            author: "Aleah Loa",
+            src: "https://scontent-b-sea.xx.fbcdn.net/hphotos-xfa1/t1.0-9/488150_4394334106398_428929546_n.jpg",
+            caption: "July 18th, 2013"
+        }
+    ];
     ];
 }
