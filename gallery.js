3468c259a1291b4c25e5c0a47155eb226044605c
aleahloa <aleahloa@yahoo.ca>
2014-06-02 21:18:50 -0700

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -4,7 +4,7 @@ function GalleryCtrl($scope) {
             title: "Bubblegum Jets",
             author: "Aleah Loa",
             src: "https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-xaf1/t31.0-8/10339318_10203746067402063_1630967137937098385_o.jpg",
-            caption: "Bubblegum Jets"
+            caption: "May 2nd, 2014"
         }
     ];
 }
