faba4af1123fd9346a09edb3a1479ed9b088707b
aleahloa <aleahloa@yahoo.ca>
2014-06-04 14:52:29 -0700

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -30,24 +30,7 @@ function GalleryCtrl($scope) {
             src: "https://scontent-b-sea.xx.fbcdn.net/hphotos-xfa1/t1.0-9/488150_4394334106398_428929546_n.jpg",
             caption: "July 18th, 2013"
         },
-        {
-            title: "Cotton Candy",
-            author: "Aleah Loa",
-            src: "https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-xpf1/t31.0-8/s720x720/324693_2251028325093_293588_o.jpg",
-            caption: "August 22nd, 2011"
-        },
-        {
-            title: "Adhesion",
-            author: "Aleah Loa",
-            src: "https://scontent-b-sea.xx.fbcdn.net/hphotos-xfa1/t31.0-8/s720x720/280065_2199938687884_2142537_o.jpg",
-            caption: "August 1st, 2011"
-        },
-        {
-            title: "Midnight City",
-            author: "Aleah Loa",
-            src: "https://scontent-a-sea.xx.fbcdn.net/hphotos-xfp1/t31.0-8/s720x720/169346_4010105900933_1933762236_o.jpg",
-            caption: "June 15th, 2012"
-        },
+      
         {
             title: "Sleepy Prayers",
             author: "Aleah Loa",
