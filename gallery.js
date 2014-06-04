d18d1ea857028f9ba6cd049d82dc329f4ecda14b
Jonny Li <myself@jonny.li>
2014-06-04 16:46:18 -0700

    I'm OCD about unnecessary spaces...

--- gallery.js
+++ gallery.js
@@ -1,20 +1,17 @@
 function GalleryCtrl($scope) {
     $scope.images = [
-        
         {
             title: "Untitled",
             author: "Aleah Loa",
             src: "https://fbcdn-sphotos-e-a.akamaihd.net/hphotos-ak-xpf1/t1.0-9/10452291_10203757774134724_4674911474546280008_n.jpg",
             caption: "May 31st, 2014"
         },
-        
         {
             title: "Bubblegum Jets",
             author: "Aleah Loa",
             src: "https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-xaf1/t31.0-8/10339318_10203746067402063_1630967137937098385_o.jpg",
             caption: "May 2nd, 2014"
         },
-        
         {
 
             title: "Parisian Nights",
@@ -22,24 +19,19 @@ function GalleryCtrl($scope) {
             src: "https://scontent-b-sea.xx.fbcdn.net/hphotos-xaf1/t1.0-9/1069960_10201394272448659_847322368_n.jpg",
             caption: "July 18th, 2013"
         },
-        
         {
             title: "Cathédrale Notre Dame de Paris",
             author: "Aleah Loa",
             src: "https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-xfa1/t1.0-9/998867_10201394491734141_1744311596_n.jpg",
             caption: "July 18th, 2013"
         },
-        
-      
         {
             title: "Sleepy Prayers",
             author: "Aleah Loa",
             src: "https://scontent-a-sea.xx.fbcdn.net/hphotos-xaf1/t31.0-8/s720x720/192804_4370784637676_1422868768_o.jpg",
             caption: "September 3rd, 2012"
         },
-      
-        
-         {
+        {
             title: "Focus",
             author: "Aleah Loa",
             src: "https://scontent-b-sea.xx.fbcdn.net/hphotos-xap1/t31.0-8/615010_10202058765820578_1091587269_o.jpg",
