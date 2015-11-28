0c1ab5d8948322c9458a6b1d60eb7487097fb257
aleahloa <aleahloa@yahoo.ca>
2015-11-27 22:59:06 -0800

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -2,88 +2,40 @@
 function GalleryCtrl($scope) {
     $scope.images = [
         {
-            title: "Stage 2 of Self Direction",
-            author: "Aleah Loa",
-            src: "https://i.imgur.com/dtLSqSi.jpg",
-            caption: "March 31st, 2015"
+            title: "",
+            author: "",
+            src: "https://imgur.com/BcrSZDQ",
+            caption: ""
         },
         {
-            title: "Automation",
-            author: "Aleah Loa",
-            src: "https://i.imgur.com/NTu5Lt2.jpg",
-            caption: "February 24th, 2015"
+            title: "",
+            author: "",
+            src: "https://imgur.com/CjApVnI",
+            caption: ""
         },
         {
-            title: "Distressed",
-            author: "Aleah Loa",
-            src: "https://i.imgur.com/vSpNNFW.jpg",
-            caption: "February 9th, 2015"
+            title: "",
+            author: "",
+            src: "https://imgur.com/CjApVnI",
+            caption: "https://imgur.com/FWrNxEh"
         },
         {
-            title: "My Interpretation",
-            author: "Aleah Loa",
-            src: "https://i.imgur.com/fOQPDYG.jpg",
-            caption: "February 2nd, 2015"
+            title: "",
+            author: "",
+            src: "https://imgur.com/CjApVnI",
+            caption: "https://imgur.com/mHMt9tL"
         },
         {
-            title: "Carve Us",
-            author: "Aleah Loa",
-            src: "https://i.imgur.com/owh2XWc.jpg",
-            caption: "January 5th, 2015"
+            title: "",
+            author: "",
+            src: "https://imgur.com/CjApVnI",
+            caption: "https://imgur.com/wgJApoU"
         },
         {
-            title: "Waxflow",
-            author: "Aleah Loa",
-            src: "https://i.imgur.com/ejYLLF8.jpg",
-            caption: "January 5th, 2015"
-        },
-        {
-            title: "Shattered",
-            author: "Aleah Loa",
-            src: "https://i.imgur.com/3IkErPf.jpg",
-            caption: "December 12th, 2014"
-        },
-        {
-            title: "Bloody Daggers",
-            author: "Aleah Loa",
-            src: "https://i.imgur.com/m6o454V.jpg",
-            caption: "November 11th, 2014"
-        },
-        {
-            title: "Cat's Spine + Pelvis",
-            author: "Aleah Loa",
-            src: "https://i.imgur.com/S17kzdJ.jpg?1",
-            caption: "October 28th, 2014"
-        },
-        {
-            title: "Primary",
-            author: "Aleah Loa",
-            src: "https://i.imgur.com/6QdAZyO.jpg",
-            caption: "May 31st, 2014"
-        },
-        {
-            title: "DJ Hanzel",
-            author: "Aleah Loa",
-            src: "https://i.imgur.com/udQraBA.jpg",
-            caption: "May 29th, 2014"
-        },
-        {
-            title: "Bubblegum Jets",
-            author: "Aleah Loa",
-            src: "https://i.imgur.com/obUh9YL.jpg",
-            caption: "May 2nd, 2014"
-        },
-        {
-            title: "Experimentation",
-            author: "Aleah Loa",
-            src: "https://i.imgur.com/l5qjnvc.jpg",
-            caption: "April 24th, 2014"
-        },
-        {
-            title: "Ants",
-            author: "Aleah Loa",
-            src: "https://i.imgur.com/ovmiXl1.jpg",
-            caption: "September 21st, 2012"
+            title: "",
+            author: "",
+            src: "https://imgur.com/CjApVnI",
+            caption: "https://imgur.com/yg9RtXI"
         }
     ];
 }
