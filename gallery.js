6a6e87851a647460eea598138e8437d7bbbfafe8
aleahloa <aleahloa@yahoo.ca>
2016-06-15 20:21:34 -0700

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,11 +1,5 @@
 // use https links for imgur links pls, meaning if ur link starts with http://, change it to start with https://
 function GalleryCtrl($scope) {
     $scope.images = [
-        {
-            title: "",
-            author: "",
-            src: "",
-            caption: ""
-        }
     ];
 }
